82eb4437b848e27ea54af86cc1d2d91edb8bdaad
Dainis Dilevka <thedenisovan@proton.me>
2025-08-27 15:07:18 +0100

    Add: Slide section of main page header

--- src/module/mainPage/mainDom.js
+++ src/module/mainPage/mainDom.js
@@ -1,6 +1,9 @@
+let currElIdx = 0;
+
 const els = {
   navbar: document.querySelector('[data-nav]'),
   currUsername: document.querySelector('[data-curr-user]'),
+  nextInfoSlide: document.querySelector('[data-next-info]')
 }
 
 els.currUsername.textContent = localStorage.getItem('currActiveUsername');
@@ -28,4 +31,15 @@ function bindHeaderEvents() {
   });
 }
 
+function domCarousel() {
+  currElIdx === 3 ? currElIdx = 0 : currElIdx++;
+  let children = document.querySelectorAll('.info-child');
+  children.forEach((child) => child.classList.add('hidden'));
+  document.querySelector(`[data-info="${currElIdx}"]`).classList.remove('hidden');
+}
+
+els.nextInfoSlide.addEventListener('click', domCarousel);
+
 bindHeaderEvents();
+
+// TODO: Add functionality witch will look on current screen size and scroll slides based on it
