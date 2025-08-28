af5a065da9e9c787009de6a16c1ba36a9e54cebf
Dainis Dilevka <thedenisovan@proton.me>
2025-08-28 14:03:15 +0100

    Add: glidejs package for sliding carousel elements

--- src/module/mainPage/index.js
+++ src/module/mainPage/index.js
@@ -4,3 +4,26 @@ import 'normalize.css';
 import '../../style/cssUtils/reset.scss';
 import '../../style/style.scss';
 import '../../style/cssUtils/cube.css';
+import Glide from '@glidejs/glide';
+import '@glidejs/glide/dist/css/glide.core.min.css';
+import '@glidejs/glide/dist/css/glide.theme.min.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const glide = new Glide('.glide', {
+  type: 'carousel',
+  startAt: 0,
+  perView: 3,
+  breakpoints: {
+    1024: {
+      perView: 2
+    },
+    600: {
+      perView: 1
+    }
+  },
+  gap: 15,
+  autoplay: 3000
+});
+
+  glide.mount();
+});
--- src/module/mainPage/mainDom.js
+++ src/module/mainPage/mainDom.js
@@ -3,8 +3,6 @@ let frontSlideIdx = 0;
 const els = {
   navbar: document.querySelector('[data-nav]'),
   currUsername: document.querySelector('[data-curr-user]'),
-  nextInfoSlide: document.querySelector('[data-next-info]'),
-  previousInfoSlide: document.querySelector('[data-back-info]')
 }
 
 els.currUsername.textContent = localStorage.getItem('currActiveUsername');
@@ -29,26 +27,7 @@ function bindHeaderEvents() {
       ) {
       toggleSingleClass(els.navbar, 'visible');
     }
-    // Events attached to buttons to move head slides back and forth
-    if (target.closest('[data-back-info]')) {
-      mobileCarousel(false);
-    }
-    if (target.closest('[data-next-info]')) {
-      mobileCarousel();
-    }
   });
-}
-
-// Makes slides move on mobile mobile device screens size
-function mobileCarousel(moveForward = true) {
-  if(moveForward) {
-    frontSlideIdx === 3 ? frontSlideIdx = 0 : frontSlideIdx++;
-  } else {
-    frontSlideIdx === 0 ? frontSlideIdx = 3 : frontSlideIdx--;
-  }
-  let children = document.querySelectorAll('.info-child');
-  children.forEach((child) => child.classList.add('hidden'));
-  document.querySelector(`[data-info="${frontSlideIdx}"]`).classList.remove('hidden');
 }
 
 bindHeaderEvents();
