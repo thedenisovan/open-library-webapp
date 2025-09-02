41487f71f8c0df6e114b156f8598a0d42f00dc73
Dainis Dilevka <thedenisovan@proton.me>
2025-09-02 19:58:55 +0100

    Clean: replaced hardcoded html to dynamic js

--- src/module/mainPage/domCreation.js
+++ src/module/mainPage/domCreation.js
@@ -1,24 +1,61 @@
 import * as cl from '../common/bookClass';
-const loadingSvg = require('../../assets/loading-svgrepo-com.svg');
-
-// Makes html containers for each book
-export function makeBookShelf(shelfLength, bookShelf) {
-  if (!shelfLength || !bookShelf) return null;
-
-  // Parent element of whole book genre
-  const shelf = document.querySelector(bookShelf);
-
-  for (let i = 0; i < shelfLength; i++) {
-    let book = document.createElement('li');
-    let cover = document.createElement('img');
-    book.classList.add('glide__slide');
-    book.id = `book${i}`;
-
-    book.appendChild(cover);
-    shelf.appendChild(book);
-  }
+const svgRepo = {
+  loadingSvg: require('../../assets/loading-svgrepo-com.svg'),
+  arrowLeft: require('../../assets/MainHead/previous-svgrepo-com.svg'),
+  arrowRight: require('../../assets/MainHead/next-svgrepo-com.svg')
 }
 
+// Makes html containers for each book and adds gliderJs html classes 
+export function makeBookShelf(parent) {
+  const container = document.querySelector(parent);
+
+  const track = document.createElement("div");
+  track.className = "glide__track";
+  track.setAttribute("data-glide-el", "track");
+
+  const ul = document.createElement("ul");
+  ul.className = "glide__slides";
+
+  // For each glide slide create 10 
+  for (let i = 0; i < 10; i++) {
+    const li = document.createElement("li");
+    li.className = "glide__slide";
+    li.id = `book${i}`;
+
+    const img = document.createElement("img");
+    li.appendChild(img);
+
+    ul.appendChild(li);
+  }
+
+  track.appendChild(ul);
+
+  const arrows = document.createElement("div");
+  arrows.className = "glide__arrows";
+  arrows.setAttribute("data-glide-el", "controls");
+
+  const leftBtn = document.createElement("button");
+  const leftBtnImg = document.createElement('img');
+  leftBtn.appendChild(leftBtnImg);
+  leftBtn.className = "glide__arrow glide__arrow--left";
+  leftBtn.setAttribute("data-glide-dir", "<");
+  leftBtnImg.src = svgRepo.arrowLeft;
+
+  const rightBtn = document.createElement("button");
+  const rightBtnImg = document.createElement('img');
+  rightBtn.appendChild(rightBtnImg);
+  rightBtn.className = "glide__arrow glide__arrow--right";
+  rightBtn.setAttribute("data-glide-dir", ">");
+  rightBtnImg.src = svgRepo.arrowRight;
+
+  arrows.appendChild(leftBtn);
+  arrows.appendChild(rightBtn);
+
+  container.appendChild(arrows);
+  container.appendChild(track);
+}
+
+// Gets cover from open lib database and displays it to screen
 async function displayCovers(glideIdx, books) {
   displayRotationSvg(glideIdx);
   for (let i = 0; i < books.length; i++) {
@@ -38,20 +75,23 @@ async function displayCovers(glideIdx, books) {
   }
 }
 
+// While async function gets cover show rotating svg
 function displayRotationSvg(glideIdx) {
   document
     .querySelectorAll(
       `.glide${glideIdx} .glide__track .glide__slides .glide__slide img`
     )
     .forEach((cover) => {
-      cover.setAttribute('src', loadingSvg);
+      cover.setAttribute('src', svgRepo.loadingSvg);
       cover.classList.add('rotate');
     });
 }
 
 // Renders book shelf with position for 10 books
-for (let i = 2; i < 7; i++) {
-  makeBookShelf(10, `.glide${i} .glide__track .glide__slides`)
+const minContainerIdx = 2;
+const maxContainerIdx = 7;
+for (let i = minContainerIdx; i < maxContainerIdx; i++) {
+  makeBookShelf(`.glide${i}`)
 }
 
 async function getBookData(title, author) {
